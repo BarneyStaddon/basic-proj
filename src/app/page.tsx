66dda4d90aabeed5c5eb52fe92fed73480568966
BarneyStaddon <barneystaddon@yahoo.com>
2024-07-16 09:41:12 +0100

    Pass stream url as prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import AudioPlayer from "./components/AudioPlayer";
 
 export default function Page() {
-  return  <AudioPlayer />
+  return  <AudioPlayer data="http://167.172.56.234:8000/stream.mp3"/>
 }
