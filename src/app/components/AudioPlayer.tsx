e382b102301781d24432476a95dc36d5f231a4aa
BarneyStaddon <barneystaddon@yahoo.com>
2024-07-16 17:36:49 +0100

    Refactor to create audio as required

--- src/app/components/AudioPlayer.tsx
+++ src/app/components/AudioPlayer.tsx
@@ -1,17 +1,45 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
 
 interface AudioPlayer {
   data: any;
 }
 
 const AudioPlayer: React.FC<AudioPlayer> = ({ data }) => {
+
+  const audioElem = useRef<HTMLAudioElement>(null);
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlay = (play: boolean) => {
+    if (audioElem.current) {
+      setPlaying(false)
+      audioElem.current.pause();
+      audioElem.current = null;
+    }
+    else {
+      setPlaying(true)
+      audioElem.current = new Audio(data);
+      audioElem.current.play();
+    }
+
+  };
+
   return (
-    <figure>
-        <figcaption>Play here</figcaption>
-        <audio controls src={data}></audio>
-    </figure>
+    <Card variant="outlined" 
+          sx={{ maxWidth: 345 }}>
+      <CardContent>
+          <h1>Electric Dreams radio POC</h1>
+          <ButtonGroup variant="contained">
+            <Button variant="contained" onClick={() => handlePlay(true)} >{playing ? 'PAUSE' : 'LISTEN'}</Button>
+          </ButtonGroup>
+      </CardContent>
+    </Card>
   );
 };
 
